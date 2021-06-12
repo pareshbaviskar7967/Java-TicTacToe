98b94db38be36ef83e8e2878698707ee6577ad69
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-06-12 08:39:34 +0530

    Solution for UC-1

--- tictactoe/TicTacToeGame.java
+++ tictactoe/TicTacToeGame.java
@@ -1,7 +1,21 @@
 package tictactoe;
 
+import java.util.*;
+import java.lang.*;
+
 public class TicTacToeGame {
+
+	public static char[] createBoard() {
+		char[] board=new char[10];
+		for(int i=1;i<board.length;i++) {
+			board[i]=' ';
+		}
+		return board;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Heyyy.....Welcome to the TicTacToe Game");
+		
+		char[] board=createBoard();
 	}
 }
